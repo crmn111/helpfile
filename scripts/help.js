4403a96282944c9d0a5c0295f4f388977a3b443b
Dirk-Jan <dirkjan@degroot.in>
2013-06-07 12:14:56 +0200

    help system version 0.3

--- scripts/help.js
+++ scripts/help.js
@@ -12,44 +12,139 @@
  *
  * Author: dirkjan@degroot.in
  * Location: https://github.com/dirkjan111/helpfile
- * Version: 0.2
+ * Version: 0.3
  * Date: 03-11-2011
  */
 
+try {
+    var version = window.location.search.substring(1);
+    var version = version.split("version=");
 
-var version = window.location.search.substring(1);
-var version = version.split("version=");
+    var appName = 'Appname';
+    var appVersion = version[1] || '2.0';
+    var appTitle = appName + ' ' + appVersion;
+    var topPanel = '142';
+} catch(e) {}
 
-var appName = 'Appname';
-var appVersion = version[1] || '2.0';
-var appTitle = appName + ' ' + appVersion;
 
-function goToByScroll(id){
+function goToByScroll(id, callback){
 	function m(tr) {
 		return Math.round(tr);
 	}
     try {
+        var callback = (callback) ? callback : $.noop;
         var header = $('#start').height() + $('div.bar').height();
         var realtop = (id === '#start') ? 0 : $(id).offset().top;
         var scrolltop = (id === '#start') ? 0 : $("div.col-2 div.head").scrollTop();
         var xtop = m(scrolltop + realtop)-2;
         $(id).parent("div.section").effect("highlight", {}, 3000);
-        $('div.col-2 div.head').animate({scrollTop: (xtop-header-5)},250);
+        $('div.col-2 div.head').animate({scrollTop: (xtop-header-5)},250, callback);
     } catch(e) {}
 }
 
-function checkWidth() {
+var checkWidth = function() {
     var windowWidth = $(window).width();
     var dl = $("div.bar dl");
     (windowWidth < 1250) ? dl.hide() : dl.show();
-}
+};
+
+var resizeSplitter = function() {
+    var height = $(window).height()-2;
+    var width = $(window).width();
+    var splitterElement = $("#splitter"),
+    splitterObject = splitterElement.data("kendoSplitter");
+    splitterElement.css({width: width + 'px', height: height + 'px' });
+    splitterObject.trigger("resize");
+};
+
+var toggleHighLightArea = function(type) {
+    /* if highlight area is hidden, then ease in */
+    var marginRight = (type==='show') ? 67 : 0;
+    var base = $('div.hlight');
+
+    $('.col-2 div.contents').animate({
+        marginRight : marginRight + 'px'
+    },200);
+};
+
+var drawMarkers = function() {
+    /* draw the markers in the highlight area */
+
+    /* height of the header: */
+    var header = 118;
+
+    /* remove the highlights */
+    $('div.hlight a').remove();
+
+    toggleHighLightArea('show');
+
+    var proceed = function() {
+        var base = $('.col-2 div.contents > div');
+        var scrolltop =$('.col-2 div.contents');
+
+
+        /* see how big are the canvases */
+        var heightFixed = $('div.hlight').height();
+        var heightFlex = base.height();
+
+        /* fill the array with size objects */
+        var arrayOfHighlightsRatios = [];
+
+        $('span.highlight').each(function(k,v){
+            var topPos = Math.floor($(this).offset().top) - header;
+            var obj = {
+                flex: topPos,
+                fix: Math.floor(topPos/heightFlex*heightFixed),
+                text: $(this).text()
+            };
+            arrayOfHighlightsRatios.push(obj);
+        });
+
+        /* add and animate the highlights */
+        $.each(arrayOfHighlightsRatios, function(k,v) {
+            $('div.hlight').append('<a href="#" id="m'+k+'" class="marker" title="'+ v.text+'" style="top:'+ v.fix+'px" rel="'+ v.flex+'"></a>');
+            $('div.hlight m'+k).hide().delay(200).fadeIn('medium');
+        });
+
+        $(document).off('click','div.hlight a.marker').on('click','div.hlight a.marker', function() {
+           var rel = $(this).attr('rel');
+            $(this).parent().find('a').removeClass('focus');
+            $(this).addClass('focus');
+            $('div.col-2 div.head').animate({scrollTop: rel},250);
+            return false;
+        });
+    }
+
+    /* scroll to top */
+    goToByScroll('#start', proceed);
+};
+
 
 $(window).resize(function () {
     checkWidth();
+    resizeSplitter();
+    drawMarkers();
 });
 
 $(document).ready(function () {
+
+    $("#splitter").kendoSplitter({
+        orientation: "horizontal",
+        panes: [
+            { collapsible: true, resizable:true, min:'300px', max:'500px', size:'19%' },
+            { collapsible: false }
+        ]
+    });
+
     checkWidth();
+    resizeSplitter();
+
+    //focus the widget
+    $(document).on("keydown.examples", function(e) {
+        if (e.altKey && e.keyCode === 87 /* w */) {
+            $("#splitter").find(".k-splitbar:first").focus();
+        }
+    });
 
 	document.title = appTitle;	
 	$("span.appname").each(function(){
@@ -94,6 +189,7 @@ $(document).ready(function () {
                         '<option value="basic">Basic</option>'+
                         '<option value="fresh">Fresh</option>'+
                         '<option value="formal">Formal</option>'+
+                        '<option value="noir">Noir</option>'+
                     '</select>'+
                 '</div>';
 	
@@ -106,7 +202,7 @@ $(document).ready(function () {
 	self.target = $('option:selected', 'select#target').val();
 	
 	$(document).on('change','.style-switcher select',function() {
-		$("body").removeClass('fresh formal basic').addClass($('option:selected', $(this)).val().toLowerCase());
+		$("body").removeClass('fresh formal basic noir').addClass($('option:selected', $(this)).val().toLowerCase());
         $.cookie('help_style', $(this).val());
 	});
 
@@ -126,12 +222,16 @@ $(document).ready(function () {
             $('.col-2').highlight(v);
             $.cookie('help_highlight', v);
         }
+        setTimeout(function() {
+            drawMarkers();
+        },1000);
+
     });
 
-    function manageZindex() {
+    var manageZindex = function() {
         var zindex = ($.trim($("div.highlight input").val()).length > 0) ? '999' : '-1';
         $("a.reset").css({'z-index' : zindex});
-    }
+    };
 
     $(document).on('keyup','div.highlight input',function(e) {
         switch(e.type) {
@@ -179,6 +279,8 @@ $(document).ready(function () {
     $('a.clear').on('click', function() {
         $('.col-2').removeHighlight();
         $.cookie('help_highlight', null);
+        $('div.hlight a').remove();
+        toggleHighLightArea('clear');
         return false;
     });
 
@@ -216,46 +318,73 @@ $(document).ready(function () {
     $(document).jkey('esc,right,left,end,home',true,function(sKey,oEvent){
         var et = oEvent.target.nodeName.toLowerCase() || null;
         var base = $('ul.filetree a.active');
+        var nrInTree = $('ul.filetree a').length;
+
+        var hlBase = $('div.hlight');
+        var nrFocus = hlBase.find('a.focus').length;
+        var nrHighlights = hlBase.find('a').length;
+
         base.removeClass('active').blur();
-        if (et !== 'input' && et !== 'textarea') {
 
-            if(sKey == 'escape') {
-                $("div.window_panel", window.parent.document).hide();
-                $("button.help", window.parent.document).removeClass('active');
-            }
-
-            if(base.length) {
-                switch(sKey){
-                    case 'right' :
-                        var basenext = (base.closest('li').find('ul').length) ? base.closest('li').find('ul li:first') : (base.closest('li').next().length) ? base.closest('li').next() : base.parents('ul.filetree > li').next();
-                        if(basenext.hasClass('expandable')) { basenext.find('.hitarea').trigger('click'); }
-                        if(base.parents('ul.filetree > li').hasClass('expandable')) { base.parents('ul.filetree > li').find('.hitarea').trigger('click'); }
-                        basenext.find('a').filter(':first').trigger('click');
-                    break;
-                    case 'left' :
-                        var baseprev = (base.closest('li').prev().length) ? (base.closest('li').prev().find('ul').length) ? base.closest('li').prev().find('ul li:last') : base.closest('li').prev() : (base.parents('ul.filetree > li').find('span.folder').length) ? base.closest('ul').prev('span.folder') : base.parents('ul.filetree > li').prev();
-                        base.closest('li').find('span.folder');
-                        if(baseprev.hasClass('expandable')) { baseprev.find('.hitarea').trigger('click'); }
-                        if(base.closest('li').hasClass('collapsable') && base.parents('span.folder').length) { base.closest('li').find('.hitarea').trigger('click'); }
-                        baseprev.find('a').filter(':first').trigger('click');
-                    break;
-                    case 'home' :
-                        $('ul.filetree a:first').trigger('click');
-                    break;
-                    case 'end' :
-                        $('ul.filetree a:last').trigger('click');
-                    break;
-                    /*case 'm' :
-                        if(oEvent.altKey) {
-                            $('a.print').trigger('click');
-                        }
-                        return false;
-                    break;*/
-                }
-            } else {
-                $('ul.filetree a:first').trigger('click');
-            }
+        if(sKey == 'escape') {
+            $("div.window_panel", window.parent.document).hide();
+            $("button.help", window.parent.document).removeClass('active');
         }
+
+        switch(sKey){
+            case 'right' :
+                if (oEvent.ctrlKey) {
+                    if(nrFocus === 0 || hlBase.find('a.focus').index()+1 === nrHighlights) {
+                        hlBase.find('a:first').trigger('click');
+                    } else {
+                        hlBase.find('a.focus').next().trigger('click');
+                    }
+                    return;
+                }
+
+                if (!oEvent.ctrlKey) {
+                    if(base.length === 0 || base.index() === nrInTree) {
+                        return $('ul.filetree a:first').trigger('click');
+                    }
+
+                    var basenext = (base.closest('li').find('ul').length) ? base.closest('li').find('ul li:first') : (base.closest('li').next().length) ? base.closest('li').next() : base.parents('ul.filetree > li').next();
+                    if(basenext.hasClass('expandable')) { basenext.find('.hitarea').trigger('click'); }
+                    if(base.parents('ul.filetree > li').hasClass('expandable')) { base.parents('ul.filetree > li').find('.hitarea').trigger('click'); }
+                    basenext.find('a').filter(':first').trigger('click');
+                }
+            break;
+            case 'left' :
+                if (oEvent.ctrlKey) {
+                    if(nrFocus === 0 || hlBase.find('a.focus').index() === 0) {
+                        hlBase.find('a:last').trigger('click');
+                    } else {
+                        hlBase.find('a.focus').prev().trigger('click');
+                    }
+                    return;
+                }
+
+                if (!oEvent.ctrlKey) {
+                    /*if(base.index() === 0) {
+                        return $('ul.filetree a:last').trigger('click');
+                    }*/
+
+                    var baseprev = (base.closest('li').prev().length) ? (base.closest('li').prev().find('ul').length) ? base.closest('li').prev().find('ul li:last') : base.closest('li').prev() : (base.parents('ul.filetree > li').find('span.folder').length) ? base.closest('ul').prev('span.folder') : base.parents('ul.filetree > li').prev();
+                    base.closest('li').find('span.folder');
+                    if(baseprev.hasClass('expandable')) { baseprev.find('.hitarea').trigger('click'); }
+                    if(base.closest('li').hasClass('collapsable') && base.parents('span.folder').length) { base.closest('li').find('.hitarea').trigger('click'); }
+                    baseprev.find('a').filter(':first').trigger('click');
+                }
+
+                break;
+            case 'home' :
+                $('ul.filetree a:first').trigger('click');
+            break;
+            case 'end' :
+                $('ul.filetree a:last').trigger('click');
+            break;
+        }
+
+
     });
 
 });
